3a5d4bf771715d96f59e4614b15bab04440e77af
KROKS13kir <arbitr13adv@gmail.com>
2022-12-01 20:44:37 +0300

    deploy new 21

--- todolist/settings.py
+++ todolist/settings.py
@@ -22,8 +22,8 @@ env = environ.Env(
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Taken environment variables from .env_example file
-environ.Env.read_env(os.path.join(BASE_DIR, '.env_example'))
+# Taken environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
