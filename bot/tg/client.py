06290b59b5969c83902f5a07b21da73696c02510
KROKS13kir <arbitr13adv@gmail.com>
2023-01-06 05:14:20 +0300

    client, dc

--- bot/tg/client.py
+++ bot/tg/client.py
@@ -3,21 +3,43 @@ from bot.tg.dc import GetUpdatesResponse, SendMessageResponse
 
 
 class TgClient:
-    def __init__(self, token: str):
+    def __init__(self, token):
         self.token = token
 
-    def get_url(self, method: str) -> str:
-        return f"https://api.telegram.org/bot{self.token}/{method}"
+    def get_url(self, method: str):
+        return f'https://api.telegram.org/bot{self.token}/{method}'
 
     def get_updates(self, offset: int = 0, timeout: int = 60) -> GetUpdatesResponse:
-        url = self.get_url("getUpdates")
-        resp = requests.get(url, params={"offset": offset, "timeout": timeout})
-        return GetUpdatesResponse.Schema().load(resp.json())
+        url = self.get_url('getUpdates')
+
+        params = {
+            "offset": offset,
+            "timeout": timeout
+        }
+        headers = {
+            "accept": "application/json",
+            "User-Agent": "Django application",
+            "content-type": "application/json"
+        }
+        response = requests.get(url=url, headers=headers, params=params)
+
+        return GetUpdatesResponse.Schema().load(response.json())
 
     def send_message(self, chat_id: int, text: str) -> SendMessageResponse:
-        url = self.get_url("sendMessage")
-        resp = requests.get(url, params={"chat_id": chat_id, "text": text})
-        return SendMessageResponse.Schema().load(resp.json())
+        url = self.get_url('sendMessage')
+
+        payload = {
+            "text": text,
+            "chat_id": chat_id
+        }
+        headers = {
+            "accept": "application/json",
+            "User-Agent": "Django application",
+            "content-type": "application/json"
+        }
+        response = requests.post(url=url, headers=headers, json=payload)
+
+        return SendMessageResponse.Schema().load(response.json())
 
 
 
--- bot/tg/dc.py
+++ bot/tg/dc.py
@@ -5,25 +5,27 @@ from marshmallow_dataclass import dataclass
 from marshmallow import EXCLUDE, Schema
 
 
+@dataclass
 class MessageFrom:
+    """Telegram API: https://core.telegram.org/bots/api#user"""
     id: int
-    is_bot: bool
-    first_name: str
-    last_name: Optional[str]
-    username: str
+    first_name: Optional[str] = field(default=None)
+    last_name: Optional[str] = field(default=None)
+    username: Optional[str] = field(default=None)
 
     class Meta:
         unknown = EXCLUDE
 
 
 @dataclass
-class MessageChat:
+class Chat:
+    """Telegram API: https://core.telegram.org/bots/api#chat"""
     id: int
-    first_name: Optional[str]
-    username: Optional[str]
-    last_name: Optional[str]
     type: str
-    title: Optional[str]
+    first_name: Optional[str] = field(default=None)
+    last_name: Optional[str] = field(default=None)
+    username: Optional[str] = field(default=None)
+    title: Optional[str] = field(default=None)
 
     class Meta:
         unknown = EXCLUDE
@@ -31,20 +33,22 @@ class MessageChat:
 
 @dataclass
 class Message:
+    """Telegram API: https://core.telegram.org/bots/api#message"""
     message_id: int
-    from_: MessageFrom = field(metadata={'data_key': 'from'})
-    chat: MessageChat
-    date: int
-    text: str
+    chat: Chat
+    # override usage of keyword "from" - add underscore and metadata to map to data key
+    from_: Optional[MessageFrom] = field(metadata=dict(data_key='from'), default=None)
+    text: Optional[str] = field(default=None)
 
     class Meta:
         unknown = EXCLUDE
 
 
 @dataclass
-class UpdateOdj:
+class UpdateObj:
+    """Telegram API: https://core.telegram.org/bots/api#getting-updates"""
     update_id: int
-    message: Message
+    message: Optional[Message] = field(default=None)
 
     class Meta:
         unknown = EXCLUDE
@@ -52,10 +56,9 @@ class UpdateOdj:
 
 @dataclass
 class GetUpdatesResponse:
+    """https://core.telegram.org/bots/api#getupdates"""
     ok: bool
-    result: List[UpdateOdj]
-
-    Schema: ClassVar[Type[Schema]] = Schema
+    result: List[UpdateObj]
 
     class Meta:
         unknown = EXCLUDE
@@ -63,10 +66,9 @@ class GetUpdatesResponse:
 
 @dataclass
 class SendMessageResponse:
+    """https://core.telegram.org/bots/api#sendmessage"""
     ok: bool
     result: Message
-
-    Schema: ClassVar[Type[Schema]] = Schema
 
     class Meta:
         unknown = EXCLUDE
