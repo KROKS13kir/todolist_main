2d169500178d4c7e32f8c8d0fd0aeb767cbded43
KROKS13kir <arbitr13adv@gmail.com>
2023-01-07 11:55:14 +0300

    bot

--- bot/models.py
+++ bot/models.py
@@ -6,7 +6,7 @@ from django.utils.crypto import get_random_string
 class TgUser(models.Model):
     tg_chat_id = models.BigIntegerField()
     tg_user_id = models.BigIntegerField(unique=True)
-    tg_username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
+    username = models.CharField(max_length=32, validators=[MinLengthValidator(5)])
     user = models.ForeignKey('core.User', null=True, on_delete=models.CASCADE)
     verification_code = models.CharField(max_length=10, unique=True)
 
